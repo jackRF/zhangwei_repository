44cc8a7a4cc435547f990b5dd81970129404031e
张伟 <1414211340@qq.com>
2015-09-28 16:01:39 +0800

    sfs

---
+++ JAVA/jack/jack-core/src/main/java/com/jack/core/permission/Permission.java
@@ -0,0 +1,14 @@
+package com.jack.core.permission;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Permission {
+	Modul modul();
+	Action action();
+}
