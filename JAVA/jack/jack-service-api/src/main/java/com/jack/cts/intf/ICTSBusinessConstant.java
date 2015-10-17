2c644c51b0f769925858429c9270197c80686aec
张伟 <1414211340@qq.com>
2015-10-17 21:07:30 +0800

    sdfsd

--- JAVA/jack/jack-service-api/src/main/java/com/jack/cts/intf/ICTSBusinessConstant.java
+++ JAVA/jack/jack-service-api/src/main/java/com/jack/cts/intf/ICTSBusinessConstant.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright (C), 2014-2015, 达信财富投资管理（上海）有限公司
+ * FileName: ICTSBusinessConstant.java
+ * Author:   zhangwei
+ * Date:     2015年10月15日 上午11:52:30
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
 package com.jack.cts.intf;
-
+/**
+ * 〈一句话功能简述〉<br> 
+ * 信审业务常量,用于service实现和Controller配置
+ *
+ * @author zhangwei
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
 public interface ICTSBusinessConstant {
 	String NS_CTS="CTS";
 	String BT_MV_LOANAPPLY_LIST="MV_LOANAPPLY_LIST";
