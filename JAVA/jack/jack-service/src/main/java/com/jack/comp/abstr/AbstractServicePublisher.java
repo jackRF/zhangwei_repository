58981008a7d78b74d57e9737caca1e88e2fe02b3
张伟 <1414211340@qq.com>
2015-10-19 01:55:12 +0800

    sfsfs

--- JAVA/jack/jack-service/src/main/java/com/jack/comp/abstr/AbstractServicePublisher.java
+++ JAVA/jack/jack-service/src/main/java/com/jack/comp/abstr/AbstractServicePublisher.java
@@ -1,16 +1,11 @@
 package com.jack.comp.abstr;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.jack.intf.observer.IEmitter;
 import com.jack.intf.observer.IPublish;
 
 public abstract class  AbstractServicePublisher<S,T,O extends IEmitter<S, T>> extends AbstractBusinessService implements IPublish<S,T>{
-//	@Autowired
-//	private List<O> observers=new ArrayList<O>();
 	protected abstract List<O> getObservers();
 	@Override
 	public <R,P>  R publish(S supportKey,T type,P param,R r){
