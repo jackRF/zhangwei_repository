8e333517d79de623a6e15369afe3726d47af66ce
张伟 <1414211340@qq.com>
2015-12-05 16:29:23 +0800

    fsfs

--- JAVA/jack/jack-web/src/main/java/com/jack/web/controller/CreditAuditController.java
+++ JAVA/jack/jack-web/src/main/java/com/jack/web/controller/CreditAuditController.java
@@ -2,7 +2,6 @@ package com.jack.web.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +11,15 @@ import com.jack.web.annotation.Namespace;
 import com.jack.web.app.CCSApplication;
 
 @Namespace(CCSApplication.NS_CTS)
-@Controller
 @RequestMapping("creditAudit")
-public class CreditAuditController {
-	@Autowired
-	private CCSApplication application;
+@Controller
+public class CreditAuditController extends AbstractController {
+	
 	@BusinessAction(businessType=CCSApplication.BT_QUERY_LOANAPPLY_LIST)
 	@ResponseBody
 	@RequestMapping("queryInfo.htm")
 	public Map<String,Object> queryInfo(Long loanApplyId){
 		return application.doBusiness(loanApplyId);
 	}
+	
 }
